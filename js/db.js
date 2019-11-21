fbfecd8cdff3e2ed23cc59d6f8993086f759ba25
ppdro-u <55445803+ppdro-u@users.noreply.github.com>
2019-11-21 08:40:46 -0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -2,18 +2,42 @@
 db.enablePersistence()
     .catch(function(err) {
         if (err.code == 'failed-precondition') {
-            console.log('persistance failed');
+            // provavelmente multiplas abas abertas ao mesmo tempo
+            console.log('Persistencia de dados falhou');
         } else if (err.code == 'unimplemented') {
-            console.log('persistance not available');
+            // browser nao suporta
+            console.log('Persistencia nao disponivel');
         }
     });
-
 db.collection('pesquisa_emprego').onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
         if (change.type === 'added') {
-            renderRecipe(change.doc.data(), change.doc.id);
+            desenhaCard(change.doc.data(), change.doc.id);
         }
         if (change.type === 'removed') {
+            // remover da pagina tambem
         }
     });
+});
+
+// adicionar nova sobremesa
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const pesquisa_emprego = {
+        nome: form.pesquisa_empregoTitulo.value,
+        descricao: form.pesquisa_empregoDescricao.value,
+        link: form.pesquisa_empregoLink.value,
+        endereco_imagem: form.pesquisa_empregoArquivo.value
+    };
+
+    db.collection('pesquisa_emprego').add(pesquisa_emprego)
+        .catch(err => console.log(err));
+
+    //reseta o formulario
+    form.pesquisa_empregoTitulo.value = '';
+    form.pesquisa_empregoDescricao.value = '';
+    form.pesquisa_empregoArquivo.value = '';
+
 });
